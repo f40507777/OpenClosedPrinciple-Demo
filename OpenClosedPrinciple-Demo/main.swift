72211144f40063106fcd2162a76bfcd393cde2ae
finnho <finnho@truetel.com>
2018-05-16 16:45:33 +0800

    Initial Commit

---
+++ OpenClosedPrinciple-Demo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  OpenClosedPrinciple-Demo
+//
+//  Created by Finn on 2018/5/16.
+//  Copyright © 2018年 Finn. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
